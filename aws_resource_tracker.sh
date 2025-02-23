ba53671b73c82d9dc6087fc441b5784ce8898e2e
swathiR7299 <swathi.r7299@gmail.com>
2025-02-23 22:18:01 +0530

    Create aws_resource_tracker.sh

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Author : Swathi
+#Date : 23-02-2025
+#Version : v1
+#This script will report the AWS resource usage
+
+set +x
+
+#list s3 buckets
+aws s3 ls
+
+#list EC2 instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId
+
+#list lambda functions
+aws lambda list-functions
+
+#list IAM users
+aws iam list-users
+
