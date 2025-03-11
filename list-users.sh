9a75d47bcdd69a559f841ec1d5923676e5e3f454
swathiR7299 <swathi.r7299@gmail.com>
2025-03-11 18:07:27 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,56 +1,42 @@
 #!/bin/bash
 
-helper()
-# Github api url
+# GitHub API URL
 API_URL="https://api.github.com"
 
-# Github username and token
-USERNAME = $username
-TOKEN = $token
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
 
-user and repo information
-REPO_OWNER = $1
-REPO_NAME = $2
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
-      local endpoint = "$1" # first argument from function
-      local url ="${API_URL}/${endpoint}"
-      
-       # Send a GET request to the GitHub API with authentication
-      curl -u "${USERNAME}:${TOKEN}" "${url}"
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
+
 # Function to list users with read access to the repository
 function list_users_with_read_access {
-        local endpoint = " /repos/{owner}/{repo}/collaborators"
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
-        # Fetch the list of collaborators on the repository
-        collaborators = "$(github_api_get "$endpoint" | jq -r '.[] | select (.permissions.pull == true) | .login')"
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
-        # Display the list of collaborators with read access
-        if [[ -z "$collaborators" ]]; then # -z unary test operator that resumes true if given string is empty
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
         echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
     else
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
-        fi
-}
-
-function helper{
-       expected_cmd_args = 2
-       #  --$#  --> number of arguments provided while giving the command
-       if [$# -ne $expected_cmd_args]; then 
-       echo "please execute the script with required cmd args"
-       echo "asd"
+    fi
 }
 
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
-
-
-        
-
-
-}
